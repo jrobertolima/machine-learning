0518be4aee45688bdbb03ed18531bbb39a3368c8
João Roberto <jbetol@gmail.com>
2018-03-27 11:58:41 -0300

    Cost function

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -12,8 +12,13 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
+%J(theta) = 1/2m*sum((h(theta)-y)^2)
+%h(theta) = theta0 - theta1x
 
-
+h = X * theta; % Matrix h(theta)
+diff_h_y = h - y; %Difference h - y =>error matrix
+diff_sqr = diff_h_y.^2; 
+J = (1/(2*m))*sum(diff_sqr);
 
 
 
