0c7423037e52c6cc05253986d4ab0b3881a2b658
Joao Roberto de Lima <jbetol@gmail.com>
2018-04-16 20:13:47 -0300

    Cost function

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -62,7 +62,7 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
-% Adding ones to X
+% Adding ones to X and only
 X = [ones(m,1) X];
 y_matrix = eye(num_labels)(y,:);
 
